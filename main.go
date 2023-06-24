73952a45ac65f8ab070871edc011ae5544d22a10
Stian Sanden Skauge <stian@skauge.org>
2023-06-24 12:13:00 +0200

    ✨ Approved test runs

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 
 	"github.com/stssk/gh-approve/models"
@@ -41,27 +43,72 @@ func main() {
 	pendingRuns := make([]models.WorkflowRun, 0)
 	pendingRunsTexts := make([]string, 0)
 	for _, run := range runs.WorkflowRuns {
-		if run.Status == "" {
+		if run.Status == "waiting" {
 			pendingRuns = append(pendingRuns, run)
-			pendingRunsTexts = append(pendingRunsTexts, fmt.Sprintf("%s, %s (%s)", run.HeadCommit.Message, run.WorkflowURL, run.HeadBranch))
-		} else {
-			fmt.Printf("%s: %s", run.Status, run.HeadCommit.Message)
+			pendingRunsTexts = append(pendingRunsTexts, fmt.Sprintf("%s, %s (%s)", run.HeadCommit.Message, run.Name, run.HeadBranch))
+			// } else {
+			// 	fmt.Printf("%s: %s", run.Status, run.HeadCommit.Message)
+			// fmt.Println("🎉 No waiting workflow runs found")
+			// return
 		}
 	}
 	// var selectedRun int
-	prompt := &survey.Select{
-		Message: "Choose a color:",
+	promptRuns := &survey.Select{
+		Message: "Select a workflow run",
 		Options: pendingRunsTexts,
 	}
 
-	var answer int
-	survey.AskOne(prompt, &answer)
+	answer := -1
+	survey.AskOne(promptRuns, &answer, survey.WithValidator(survey.Required))
+	if answer < 0 {
+		return
+	}
 
-	fmt.Println(pendingRuns[answer])
+	pendingDeployments := models.PendingDeployments{}
+	err = client.Get(models.PendingDeploymentsUrl(currentRepo.Owner(), currentRepo.Name(), pendingRuns[answer].ID), &pendingDeployments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
 
-	// survey.Ask(prompt, &selectedRun)
+	pendingDeploymentTexts := make([]string, len(pendingDeployments))
+	for i, d := range pendingDeployments {
+		pendingDeploymentTexts[i] = d.Environment.Name
+	}
 
-	// pendingDeployments := models.PendingDeployments{}
-	// err = client.Get("repos/%s/%s/actions/runs/RUN_ID/pending_deployments", currentRepo.Owner(), currentRepo.Name())
+	approvedEnvironments := []int{}
+	promptDeployments := &survey.MultiSelect{
+		Message: "Select environments to approve",
+		Options: pendingDeploymentTexts,
+	}
+	survey.AskOne(promptDeployments, &approvedEnvironments)
 
+	if len(approvedEnvironments) == 0 {
+		return
+	}
+
+	approveIds := make([]int, len(approvedEnvironments))
+	for i, e := range approvedEnvironments {
+		approveIds[i] = pendingDeployments[e].Environment.ID
+	}
+
+	approveRequest := models.RequestApprovement{
+		EnvironmentIds: approveIds,
+		State:          models.Approved,
+	}
+	req, err := json.Marshal(approveRequest)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	approvedDeployments := models.DeploymentResponse{}
+	err = client.Post(models.PendingDeploymentsUrl(currentRepo.Owner(), currentRepo.Name(), pendingRuns[answer].ID), bytes.NewReader(req), &approvedDeployments)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, e := range approvedDeployments {
+		fmt.Printf(" • %s approved @%s\n", e.Environment, e.CreatedAt)
+	}
 }
--- models/pendingDeployments.go
+++ models/pendingDeployments.go
@@ -1,19 +1,28 @@
 package models
 
-import "time"
+import (
+	"fmt"
+	"time"
+)
+
+func PendingDeploymentsUrl(owner string, repo string, runId int) string {
+	return fmt.Sprintf("repos/%s/%s/actions/runs/%d/pending_deployments", owner, repo, runId)
+}
 
 type PendingDeployments []struct {
-	Environment struct {
-		ID      int    `json:"id"`
-		NodeID  string `json:"node_id"`
-		Name    string `json:"name"`
-		URL     string `json:"url"`
-		HTMLURL string `json:"html_url"`
-	} `json:"environment"`
-	WaitTimer             int        `json:"wait_timer"`
-	WaitTimerStartedAt    time.Time  `json:"wait_timer_started_at"`
-	CurrentUserCanApprove bool       `json:"current_user_can_approve"`
-	Reviewers             []Reviewer `json:"reviewers"`
+	Environment           Environment `json:"environment"`
+	WaitTimer             int         `json:"wait_timer"`
+	WaitTimerStartedAt    time.Time   `json:"wait_timer_started_at"`
+	CurrentUserCanApprove bool        `json:"current_user_can_approve"`
+	Reviewers             []Reviewer  `json:"reviewers"`
+}
+
+type Environment struct {
+	ID      int    `json:"id"`
+	NodeID  string `json:"node_id"`
+	Name    string `json:"name"`
+	URL     string `json:"url"`
+	HTMLURL string `json:"html_url"`
 }
 
 type Reviewer struct {
@@ -66,4 +75,59 @@ type ReviewTeam struct {
 		RepositoriesURL string `json:"repositories_url"`
 		Parent          any    `json:"parent"`
 	} `json:"reviewer"`
+}
+
+type RequestApprovement struct {
+	EnvironmentIds []int  `json:"environment_ids"`
+	State          States `json:"state"`
+	Comment        string `json:"comment"`
+}
+
+type States string
+
+const (
+	Approved States = "approved"
+	Rejected States = "rejected"
+)
+
+type DeploymentResponse []struct {
+	URL                   string    `json:"url"`
+	ID                    int       `json:"id"`
+	NodeID                string    `json:"node_id"`
+	Sha                   string    `json:"sha"`
+	Ref                   string    `json:"ref"`
+	Task                  string    `json:"task"`
+	Payload               Payload   `json:"payload"`
+	OriginalEnvironment   string    `json:"original_environment"`
+	Environment           string    `json:"environment"`
+	Description           string    `json:"description"`
+	Creator               Creator   `json:"creator"`
+	CreatedAt             time.Time `json:"created_at"`
+	UpdatedAt             time.Time `json:"updated_at"`
+	StatusesURL           string    `json:"statuses_url"`
+	RepositoryURL         string    `json:"repository_url"`
+	TransientEnvironment  bool      `json:"transient_environment"`
+	ProductionEnvironment bool      `json:"production_environment"`
+}
+type Payload struct {
+}
+type Creator struct {
+	Login             string `json:"login"`
+	ID                int    `json:"id"`
+	NodeID            string `json:"node_id"`
+	AvatarURL         string `json:"avatar_url"`
+	GravatarID        string `json:"gravatar_id"`
+	URL               string `json:"url"`
+	HTMLURL           string `json:"html_url"`
+	FollowersURL      string `json:"followers_url"`
+	FollowingURL      string `json:"following_url"`
+	GistsURL          string `json:"gists_url"`
+	StarredURL        string `json:"starred_url"`
+	SubscriptionsURL  string `json:"subscriptions_url"`
+	OrganizationsURL  string `json:"organizations_url"`
+	ReposURL          string `json:"repos_url"`
+	EventsURL         string `json:"events_url"`
+	ReceivedEventsURL string `json:"received_events_url"`
+	Type              string `json:"type"`
+	SiteAdmin         bool   `json:"site_admin"`
 }
